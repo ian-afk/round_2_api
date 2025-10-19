aa4d44f3f37bac9e2f06406a8b4b5834e0a6fe90
ian-afk <crkdosal@gmail.com>
2025-10-19 19:51:00 +0800

    add findbyid , update and delete post

--- src/services/postService.ts
+++ src/services/postService.ts
@@ -10,3 +10,33 @@ export const createPost = async ({ content }) => {
 
   return post;
 };
+
+export const findPostById = async (postId: string) => {
+  const post = await Post.findById({ _id: postId });
+
+  if (!post) throw new Error('Post id does not exist');
+  return post;
+};
+
+export const updatePost = async (postId: string, { content }) => {
+  const post = await Post.findById({ _id: postId });
+
+  if (!post) throw new Error('Post id does not exists');
+  const newPost = await Post.findByIdAndUpdate(
+    {
+      _id: postId,
+    },
+    {
+      $set: content,
+    },
+    {
+      new: true,
+    },
+  );
+
+  return newPost;
+};
+
+export const deletePost = async (postId: string) => {
+  return await Post.deleteOne({ _id: postId });
+};
