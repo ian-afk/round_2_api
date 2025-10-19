0f16c0b538592ad3c302a615b88c30ac22518c2f
ian-afk <crkdosal@gmail.com>
2025-10-19 19:11:20 +0800

    add database connection

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+import { initDatabase } from './db/init';
 import { app } from './app';
 import dotenv from 'dotenv';
 
@@ -5,11 +6,13 @@ dotenv.config();
 
 void (async () => {
   try {
+    await initDatabase();
     const PORT = process.env.PORT;
     app.listen(PORT, () =>
       console.info(`express server running on http://localhost:${PORT}`),
     );
   } catch (err) {
+    console.error('connection error from database', err);
     process.exit(1);
   }
 })();
