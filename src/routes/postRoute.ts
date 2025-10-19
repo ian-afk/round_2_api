e3cb85e345361980419392e461d111d716394351
ian-afk <crkdosal@gmail.com>
2025-10-19 19:55:01 +0800

    add post route update, get by id , delete

--- src/routes/postRoute.ts
+++ src/routes/postRoute.ts
@@ -1,8 +1,16 @@
-import { createPost, getPost } from '../controller/postController';
+import {
+  createPost,
+  deletePost,
+  getPost,
+  getPostById,
+  updatePost,
+} from '../controller/postController';
 import { Router } from 'express';
 
 const router = Router();
 
 router.route('/').get(getPost).post(createPost);
+
+router.route('/:id').get(getPostById).put(updatePost).delete(deletePost);
 
 export default router;
